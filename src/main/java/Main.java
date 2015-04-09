17892c4d5c11ec1d4290e945ec80a9f592a27718
johnson-li <johnsonli1993@163.com>
2015-04-09 12:09:39 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by johnson on 4/8/15.
+ */
+public class Main {
+    static Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        logger.info(123);
+    }
+}
